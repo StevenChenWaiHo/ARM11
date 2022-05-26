631c77a10778fbb098f4b48f73a9a936d1526b41
Nixon Enraght-Moony <nixon.enraght-moony21@imperial.ac.uk>
2022-05-26 12:00:56 +0100

    Basic file reading

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,34 @@
+#include <byteswap.h> // TODO: Gnu extension
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+typedef uint32_t Instr;
+
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <input> <output>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  char *input = argv[1];
+  char *output = argv[2];
+
+  FILE *in = fopen(input, "r");
+  fseek(in, 0, SEEK_END);
+  long len = ftell(in) / sizeof(Instr);
+  fseek(in, 0, SEEK_SET);
+  // TODO: Handle errors
+
+  Instr *code = malloc(len);
+  // TODO: Handle alloc failure
+
+  fread(code, sizeof(Instr), len, in);
+  // TODO: Handle error, TOCTOU
+
+  for (int i = 0; i < len; i++)
+    code[i] = bswap_32(code[i]);
+
+  for (int i = 0; i < len; i++) {
+    printf("%d: %08x\n", i, code[i]);
+  }
 }
