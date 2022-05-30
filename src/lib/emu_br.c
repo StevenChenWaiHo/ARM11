1561976b137520ea43c667fb71bb0d979073ccfd
Rickie Ma <rm521@ic.ac.uk>
2022-05-30 15:11:43 +0100

    Emulates branch

--- src/lib/emu_br.c
+++ src/lib/emu_br.c
@@ -1,3 +1,11 @@
 #include "emu.h"
 
-void emu_br(CpuState *cpu, Instr i) {}
+void emu_br(CpuState *cpu, Instr i) {
+  Instr offset = br_offset_mask(i);
+  offset <<= 2;
+  if ((offset >> 25) & 0x1) {
+    offset |= 0xFC000000;
+  }
+  offset += 8;
+  cpu -> regs[REG_PC] += offset;
+}
