23a33b077acad612209a18dbd0e70514c30cb1df
Nixon Enraght-Moony <nixon.enraght-moony21@imperial.ac.uk>
2022-06-16 12:46:04 +0100

    asm_br: Handle backwards jumps

--- src/lib/asm_br.c
+++ src/lib/asm_br.c
@@ -15,7 +15,10 @@ Instr asm_br(Assembler *a, InstrCommon c, Instr ino) {
   if (!sym_tab_get(&a->symtab, label.source, &jno))
     assert(0); // TODO: Good error
 
-  assert(ino + 2 <= jno); // TODO: Handle backwards case:
-  Instr off = jno - ino - 2;
+  Instr off;
+  if (ino + 2 <= jno)
+    off = jno - ino - 2;
+  else
+    off = ~(ino - jno + 1) & 0xffffff;
   return bit_asm_br(off);
 }
