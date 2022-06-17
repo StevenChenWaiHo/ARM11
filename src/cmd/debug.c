331d3d0226c65d78aaa2fa54f5cc3bb5057ff56a
tyw21 <tyw21@ic.ac.uk>
2022-06-17 17:10:27 +0100

    Added assembly file (.s) support

--- src/cmd/debug.c
+++ src/cmd/debug.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "asm.h"
 #include "dbg.h"
 
 int main(int argc, char **argv) {
@@ -12,6 +13,40 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
   char *input = argv[1];
+
+  int innamelen = strlen(input) - 2;
+  char in_wo_ext[innamelen + 1];
+  strncpy(in_wo_ext, input, innamelen);
+  in_wo_ext[innamelen] = '\0';
+
+  char in_ext[2];
+  strcpy(in_ext, input + innamelen);
+  int is_s = strncmp(in_ext, ".s", 2);
+  
+  if (is_s == 0) {
+    FILE *in = fopen(input, "r");
+    if (!in) {
+      fprintf(stderr, "Failed to open %s\n", input);
+      return EXIT_FAILURE;
+    }
+    fseek(in, 0, SEEK_END);
+    long lenb = ftell(in);
+    fseek(in, 0, SEEK_SET);
+    char *buff = malloc(lenb + 1);
+    fread(buff, 1, lenb, in);
+    buff[lenb] = '\0';
+    
+    FILE *out = fopen(in_wo_ext, "w");
+    if (!out) {
+      printf("Failed to create %s\n", in_wo_ext);
+      return EXIT_FAILURE;
+    }
+    assemble(buff, input, out);
+    fclose(out);
+    fclose(in);
+    free(buff);
+    input = in_wo_ext;
+  }
 
   FILE *in = fopen(input, "r");
   if (!in) {
