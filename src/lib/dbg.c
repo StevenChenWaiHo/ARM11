eb4fb09337b933fd4266d3eab57ba1e82d2fff00
tyw21 <tyw21@ic.ac.uk>
2022-06-18 19:49:26 +0100

    Add breakpoint deletion feature

--- src/lib/dbg.c
+++ src/lib/dbg.c
@@ -19,7 +19,7 @@ static CpuCondFn condfns[] = {
 };
 
 void dbg(CpuState *cpu) {
-  int breakpoint[BREAKPOINT_NUMBER];
+  int *breakpoint = calloc(BREAKPOINT_NUMBER, sizeof(int));
   int bpt_ptr = 0;
   bool is_run;
 
@@ -34,14 +34,28 @@ void dbg(CpuState *cpu) {
         printf("Amount of breakpoints possible is (for now) limited to %d", BREAKPOINT_NUMBER);
         continue;
       }
-      int line_no = atoi(input + 2) - 1;
-      if (line_no < 0 || line_no >= MEMORY_SIZE / 4 || !cpu->mem[line_no]) {
+      int line_no = atoi(input + 2);
+      if (line_no <= 0 || line_no >= MEMORY_SIZE / 4 || !cpu->mem[line_no - 1]) {
         printf("Such line not found.\n");
         continue;
       }
       breakpoint[bpt_ptr] = line_no;
-      printf("Breakpoint %d set at line %d.\n", bpt_ptr + 1, line_no + 1);
+      printf("Breakpoint %d set at line %d.\n", bpt_ptr + 1, line_no);
       bpt_ptr++;
+    }
+    if (input[0] == 'd' && input[1] == ' ') { // command delete
+      int bpt_no = atoi(input + 2) - 1;
+      if (bpt_no < 0 || bpt_no >= BREAKPOINT_NUMBER) {
+        printf("Such breakpoint not found.\n");
+        continue;
+      }
+      int line_no = breakpoint[bpt_no];
+      if (line_no == 0) {
+        printf("Such breakpoint not found.\n");
+        continue;
+      }
+      breakpoint[bpt_no] = 0;
+      printf("Breakpoint %d removed from line %d.\n", bpt_no + 1, line_no);
     }
     if (input[0] == 'r') { // command run / continue
       is_run = true;
@@ -69,9 +83,10 @@ bool sequence(CpuState *cpu, int *breakpoint, int bpt_ptr, bool step) {
   for (;;) {
     int curr = cpu->regs[REG_PC] >> 2;
     for (int i = 0; i < bpt_ptr; i++) {
-      if (breakpoint[i] == curr && !step) {
+      if (breakpoint[i] - 1 == curr && !step) {
         printf("Breakpoint %d at line %d\n", i + 1, curr + 1);
-        print_state(cpu);
+        print_state(cpu);       
+        cpu->regs[REG_PC] += 4;
         return false;
       }
     }
