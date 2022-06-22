f72351129da0e2b100d360bb296713cca68ffeae
tyw21 <tyw21@ic.ac.uk>
2022-06-23 00:03:26 +0100

    Revert "Fix breakpoint printing"
    This reverts commit ded443bc90b0ae927cd7d2248412fb4258f2e916.

--- src/lib/dbg.c
+++ src/lib/dbg.c
@@ -145,20 +145,20 @@ void dbg(uint32_t *mem, int total_instr_no, int *instr_to_line_no) {
                BREAKPOINT_NUMBER);
         continue;
       }
-      int line_no = atoi(input + 2); // instr_no
+      int line_no = atoi(input + 2);
       found_line_no = false;
       for (int i = 0; i < total_instr_no && !found_line_no; i++) {
         if (instr_to_line_no[i] == line_no - 1) {
-          breakpoint[bpt_ptr] =
-              i + 1; // breakpoint contains instrs start from 1
+          breakpoint[bpt_ptr] = i + 1; // breakpoint contains instrs
           found_line_no = true;
         }
       }
       if (!found_line_no) {
-        printf("Cannot set breakpoint on such line.\n");
+        printf("Such line not found.\n");
         continue;
       }
-      printf("Breakpoint %d set at line %d.\n", bpt_ptr + 1, line_no);
+      printf("Breakpoint %d set at line %d.\n", bpt_ptr + 1,
+             breakpoint[bpt_ptr]);
       bpt_ptr++;
     }
     if (input[0] == 'd' && input[1] == ' ') { // command delete
@@ -167,15 +167,13 @@ void dbg(uint32_t *mem, int total_instr_no, int *instr_to_line_no) {
         printf("Such breakpoint not found.\n");
         continue;
       }
-      int instr_no =
-          breakpoint[bpt_no] - 1; // instr_no as index in instr_to_line_no
-      if (instr_no < 0) {
+      int line_no = breakpoint[bpt_no];
+      if (line_no <= 0) {
         printf("Such breakpoint not found.\n");
         continue;
       }
       breakpoint[bpt_no] = 0;
-      printf("Breakpoint %d removed from line %d.\n", bpt_no + 1,
-             instr_to_line_no[instr_no] + 1);
+      printf("Breakpoint %d removed from line %d.\n", bpt_no + 1, line_no);
     }
     if (input[0] == 'r') { // command run / continue
       is_run = true;
