4815e123fe29c6e002f46c119ac2c90afe27cb25
Nixon Enraght-Moony <nixon.enraght-moony21@imperial.ac.uk>
2022-06-23 18:44:15 +0100

    Easier Sans

--- tests/runner/conf.go
+++ tests/runner/conf.go
@@ -1,6 +1,7 @@
 package runner
 
-const base = "./src/build/bin/"
+const san = ""
+const base = "./src/build" + san + "/bin/"
 const debug = base + "debug"
 const disassemble = base + "disassemble"
 const assemble = base + "assemble"
