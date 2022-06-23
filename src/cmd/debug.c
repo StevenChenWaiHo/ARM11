ddd919d106b5389d1dca8cfcf17122aa32844df7
Nixon Enraght-Moony <nixon.enraght-moony21@imperial.ac.uk>
2022-06-23 12:30:20 +0100

    debug: Newline in usage message

--- src/cmd/debug.c
+++ src/cmd/debug.c
@@ -83,7 +83,7 @@ done:
 int main(int argc, char **argv) {
 
   if (argc != 2) {
-    fprintf(stderr, "Usage: %s <input.s>", argv[0]);
+    fprintf(stderr, "Usage: %s <input.s>\n", argv[0]);
     return EXIT_FAILURE;
   }
   char *input = argv[1];
