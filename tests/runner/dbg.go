1317f14e4dec979d6d1bdefb195d0ee9cd4ecb22
Nixon Enraght-Moony <nixon.enraght-moony21@imperial.ac.uk>
2022-06-23 16:03:42 +0100

    Add basic test runner for debugger

---
+++ tests/runner/dbg.go
@@ -0,0 +1,67 @@
+package runner
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"os"
+	"os/exec"
+	"path"
+	"time"
+)
+
+type DbgSpec struct {
+	File   string
+	Steps  []DbgStep
+	Stderr string
+}
+
+type DbgStep struct {
+	Input  string
+	Output string
+}
+
+func RunDbg(jsonPath string) (DbgSpec, error) {
+	var spec DbgSpec
+	f, err := os.ReadFile(jsonPath)
+	if err != nil {
+		return DbgSpec{}, err
+	}
+	err = json.Unmarshal(f, &spec)
+	if err != nil {
+		return DbgSpec{}, err
+	}
+
+	asmPath := path.Join(path.Dir(jsonPath), spec.File)
+
+	cmd := exec.Command("./src/build/bin/debug", asmPath)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	// stdout, err := cmd.StdoutPipe()
+	// Must(err)
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	stdin, err := cmd.StdinPipe()
+	Must(err)
+	cmd.Start()
+	oldLen := 0
+	for i, step := range spec.Steps {
+		writeAll(stdin, []byte(step.Input))
+		writeAll(stdin, []byte("\n"))
+		time.Sleep(time.Millisecond * 10) // TODO: Be better
+		out := stdout.String()
+		spec.Steps[i].Output = out[oldLen:]
+		oldLen = len(out)
+	}
+	cmd.Wait()
+	spec.Stderr = stderr.String()
+	return spec, nil
+}
+
+func writeAll(w io.Writer, b []byte) {
+	n, err := w.Write(b)
+	if n != len(b) {
+		panic(err)
+	}
+}
